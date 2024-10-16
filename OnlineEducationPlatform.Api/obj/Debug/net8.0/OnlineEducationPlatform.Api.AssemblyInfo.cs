a01d6afea43af340555d2f3e3d89caf8a2d23759
MohamedBobo13 <mo7amed.bobo13@gmail.com>
2024-10-16 22:36:22 +0300

    clone

--- OnlineEducationPlatform.Api/obj/Debug/net8.0/OnlineEducationPlatform.Api.AssemblyInfo.cs
+++ OnlineEducationPlatform.Api/obj/Debug/net8.0/OnlineEducationPlatform.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineEducationPlatform.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e75ab0b57a683d1f7861dd5ef37cdafbcd727ccb")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineEducationPlatform.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineEducationPlatform.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
